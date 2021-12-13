02a50e4868c38c948a30856c7b78df7f5754b963
robichi <rob@ichi.org>
2021-12-12 18:52:05 -0700

    WIP - specimen composable arbitrary

--- contracts/composable/modules/strategies/capabilities/ComposableArbitrary.sol
+++ contracts/composable/modules/strategies/capabilities/ComposableArbitrary.sol
@@ -1,11 +1,41 @@
 // SPDX-License-Identifier: UNLICENSED
 
 pragma solidity 0.7.6;
+pragma abicoder v2;
 
 import { ComposedStrategyState } from "../state/ComposedStrategyState.sol";
-// import { IStrategyStubs } from "../state/IStrategyStubs.sol";
+import { Composable } from "../../../Composable.sol";
 
 
-contract ComposableArbitrary is ComposedStrategyState {
+contract ComposableArbitrary is ComposedStrategyState, Composable {
+
+    constructor(address oneTokenFactory_, address oneToken_, string memory description_)
+        ComposedStrategyState(oneTokenFactory_, oneToken_, description_)
+    {
+        registerFunction("executeTransaction(address,uint256,string memory,bytes memory)", true);
+    }
+
+    /**
+    @notice Governance can work with collateral and treasury assets. Can swap assets.
+    @param target address/smart contract you are interacting with
+    @param value msg.value (amount of eth in WEI you are sending. Most of the time it is 0)
+    @param signature the function signature
+    @param data abi-encodeded bytecode of the parameter values to send
+    */
+
+    function executeTransaction(address target, uint256 value, string memory signature, bytes memory data) external onlyOwner returns (bytes memory) {
+        bytes memory callData;
+
+        if (bytes(signature).length == 0) {
+            callData = data;
+        } else {
+            callData = abi.encodePacked(bytes4(keccak256(bytes(signature))), data);
+        }
+
+        // solium-disable-next-line security/no-call-value
+        (bool success, bytes memory returnData) = target.call{ value: value }(callData);
+        require(success, "OneTokenV1::executeTransaction: Transaction execution reverted.");
+        return returnData;
+    }
 
 }
